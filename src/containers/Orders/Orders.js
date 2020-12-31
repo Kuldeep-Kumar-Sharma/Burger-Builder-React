b3705aa7707ba19818c2fcc2cda90f9889c78d3c
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2020-12-31 23:06:57 +0530

    removed the links

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -12,7 +12,7 @@ class Orders extends Component {
 
   componentDidMount() {
     axios
-      .get("https://telebot-9de6e.firebaseio.com/orders.json")
+      .get("/orders.json")
       .then((res) => {
         const fetchedOrders = [];
         for (let key in res.data) {
